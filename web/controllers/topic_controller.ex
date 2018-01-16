d71520bb13b289ffbea006605b512de803e050cc
Dawson M <dawson.mortenson@gmail.com>
2018-01-16 10:09:33 -0800

    add handle for incomplete form submissions on the new topic route and add some styling to the the error message

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -5,13 +5,24 @@ defmodule Discuss.TopicController do
     # alias Discuss.Topic, as: Topic
     alias Discuss.Topic
 
+    # Renders a page with a form to make a new Topic
     def new(conn, _params) do
+        # Validates the params
         changeset = Topic.changeset(%Topic{}, %{})
 
         render conn, "new.html", changeset: changeset
     end
 
+
+    
     def create(conn, %{ "topic" => topic }) do
-        IO.puts(conn.request_path)
+        changeset = Topic.changeset(%Topic{}, topic)
+
+       case Repo.insert(changeset) do
+            { :ok, post } -> IO.inspect(post)
+            { :error, changeset } -> 
+                render conn, "new.html", changeset: changeset
+       end
+        # IO.puts(conn.request_path)
     end
 end
