51dd5863a12653bfb4c967a37984777763e3dee9
Dawson M <dawson.mortenson@gmail.com>
2018-01-19 16:29:32 -0800

    add comment broadcasting to let subscribed clients know a new comment was add and add render comment funcs to socket.js to respond to broadcasts

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -5,17 +5,45 @@ let socket = new Socket("/socket", { params: { token: window.userToken } })
 socket.connect()
 
 const socketToServer = (topicId) => {
-
   let channel = socket.channel(`comments:${topicId}`, {})
-  channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+  channel
+    .join()
+    .receive("ok", resp => {
+      renderComments(resp.comments);
+    })
+    .receive("error", resp => {
+      console.log("Unable to join", resp)
+    });
+
+    channel.on(`comments:${topicId}:new`, renderComment)
 
   document.querySelector("button").addEventListener("click", () => {
     const content = document.querySelector('textarea').value;
 
     channel.push('comment:add', { content: content })
   })
+}
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return commentTemplate(comment);
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+function renderComment( { comment } ) {
+  const renderedComment = commentTemplate(comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  return `
+    <li class="collection-item">
+      ${comment.content}
+    </li>
+  `
 }
 
 window.socketToServer = socketToServer
